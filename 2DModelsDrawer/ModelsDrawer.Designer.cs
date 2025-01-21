1a8566bf21ef021cd3597a3a83816a23fa00b81d
Michal Niedzwiecki <michal.niedzwiecki@workai.com>
2025-01-21 17:11:44 +0100

    git init

---
+++ 2DModelsDrawer/ModelsDrawer.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace _2DModelsDrawer
+{
+    partial class ModelsDrawer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // ModelsDrawer
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1193, 675);
+            Name = "ModelsDrawer";
+            Text = "ModelsDrawer";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ 2DModelsDrawer/ModelsDrawer.cs
@@ -0,0 +1,10 @@
+namespace _2DModelsDrawer
+{
+    public partial class ModelsDrawer : Form
+    {
+        public ModelsDrawer()
+        {
+            InitializeComponent();
+        }
+    }
+}
